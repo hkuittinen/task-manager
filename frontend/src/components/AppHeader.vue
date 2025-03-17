e357c900ce995b28ec292f7cecfa175089d10d00
Harri Kuittinen <harri.kuittinen@gmail.com>
2025-03-17 20:03:11 +0200

    feat: Add some colors

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -11,7 +11,14 @@ defineProps<{
 </template>
 
 <style scoped>
+header {
+    padding: 16px;
+    border-bottom: 4px solid var(--color-primary);
+}
+
 h1 {
     font-size: 2rem;
+    margin: 0;
+    color: #fff;
 }
 </style>
